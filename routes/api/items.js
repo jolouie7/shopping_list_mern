f0d2b0daee041ca3c7897316c4e6bb649406b09c
Your_Name <joseph161216@gmail.com>
2020-09-21 19:40:10 -0700

    add items CRUD

--- routes/api/items.js
+++ routes/api/items.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Item = require("../../models/Item");
 
 // @route GET api/items
-// @desc Get All items
+// @desc Get All Items
 // @access Public
 router.get("/", (req, res) => {
   try {
@@ -16,8 +16,38 @@ router.get("/", (req, res) => {
       .sort({ date: -1 }) // sort in descending order
       .then((items) => res.json(items));
   } catch (error) {
-    res.status(400).json('Error: ' + error)
+    res.status(400).json("Error: " + error);
   }
-})
+});
+
+// @route POST api/items
+// @desc Create an Item
+// @access Public
+router.post("/", (req, res) => {
+  try {
+    const newItem = new Item({
+      name: req.body.name,
+    });
+    newItem
+      .save()
+      .then((item) => res.json(item))
+      .catch((err) => res.status(400).json({ error: err }));
+  } catch (error) {
+    res.status(400).json("Error: " + error);
+  }
+});
+
+// @route Delete api/items/:id
+// @desc Delete an Item
+// @access Public
+router.delete("/:id", (req, res) => {
+  try {
+    Item.findByIdAndDelete(req.params.id)
+      .then(() => res.json("Item deleted!"))
+      .catch((err) => res.status(404).json({ error: err }));
+  } catch (error) {
+    res.status(400).json("Error: " + error);
+  }
+});
 
 module.exports = router;
