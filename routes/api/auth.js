62317cb9ed5a75388d3df7052088b9f2fb87cf12
Your_Name <joseph161216@gmail.com>
2020-09-27 23:05:13 -0700

    update users route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -48,6 +48,58 @@ router.post("/login", (req, res) => {
   });
 });
 
+// @route POST api/users/register
+// @desc Register new user
+// @access Public
+router.post("/register", (req, res) => {
+  const { name, email, password, username } = req.body;
+
+  // Simple validation
+  if (!name || !email || !password || !username) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  // Check for existing user
+  User.findOne({email: email, username: username}).then((user) => {
+    console.log("users error: ", user)
+    if (user) return res.status(400).json({ msg: "User already exist" });
+
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password,
+    });
+
+    // Create salt & hash
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser.save().then((user) => {
+          jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET,
+            { expiresIn: 86400 }, // expires in 24hours
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email,
+                  username: user.username,
+                },
+              })
+            }
+          );
+        });
+      });
+    });
+  });
+});
+
 // @route GET api/auth/user
 // @desc Get user data
 // @access Private
